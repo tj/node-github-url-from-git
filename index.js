9a310f6e2b00333dc8e8409e67fdb5a44c1627f2
isaacs <i@izs.me>
2013-04-19 18:49:40 -0700

    Add support for gist urls

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+var re = /^(?:https?:\/\/|git:\/\/)?(?:[^@]+@)?(gist.github.com|github.com)[:\/]([^\/]+\/[^\/]+?|[0-9]+)$/
 
 module.exports = function(url){
   try {
-    var m = /^.*?github\.com[:\/]([^\/]+)\/([^\/.]+)(?:\.git)?/.exec(url);
-    var user = m[1];
-    var repo = m[2];
-    return 'https://github.com/' + user + '/' + repo;
+    var m = re.exec(url.replace(/\.git$/, ''));
+    var host = m[1];
+    var path = m[2];
+    return 'https://' + host + '/' + path;
   } catch (err) {
     // ignore
   }
