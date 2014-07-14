015b9ac9048de423dbb851341929324eb2445ee7
bcoe <bencoe@gmail.com>
2014-07-14 16:39:50 -0700

    found replacement a bit weird.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var githubRe = /^(?:https?:\/\/|git:\/\/)?(?:[^@]+@)?(GITHUB_URLS)[:\/]([^\/]+\/[^\/]+?|[0-9]+)$/
-
 module.exports = function(url, gheUrls){
   try {
+    // whitelist of URLs that should be treated as GitHub repos.
     var githubUrls = ['gist.github.com', 'github.com'].concat(gheUrls || []);
-    var re = new RegExp(githubRe.source.replace(
-      'GITHUB_URLS', githubUrls.join('|'))
-    );
-    var m = re.exec(url.replace(/\.git$/, ''));
+    // build regex from whitelist.
+    var githubRe = new RegExp(
+      '^(?:https?:\\/\\/|git:\\/\\/)?(?:[^@]+@)?(' +
+      githubUrls.join('|') +
+      ')[:\\/]([^\\/]+\\/[^\\/]+?|[0-9]+)$'
+    )
+    var m = githubRe.exec(url.replace(/\.git$/, ''));
     var host = m[1];
     var path = m[2];
+
     return 'https://' + host + '/' + path;
   } catch (err) {
     // ignore
